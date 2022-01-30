ef166840d98726ae720a6ca40e59094c8b935ecb
1Kapustuk <98702081+1Kapustuk@users.noreply.github.com>
2022-01-30 20:20:42 +0300

    Add files via upload
    Lesson

---
+++ Lesson1.py
@@ -0,0 +1,23 @@
+# # 1 задание
+# duration = int(input('Введите кол-во секунд: '))
+# days = (duration // 86400) % 7
+# hour = (duration // 3600) % 24
+# minute = (duration // 60) % 60
+# second = duration % 60
+# print('Ваше время: ' +str(days)+ ' дн ' +str(hour)+ ' час '+str(minute)+' мин '+str(second)+ ' сек')
+
+# 2 задание, у меня к сожалению не получилось, его выполнить
+
+# 3 задание
+# procent = str ()
+# numbs = {11,12,13,14}
+# for procent in range (100):
+#     procent = procent + 1
+#     if (procent) in numbs:
+#         print((procent), 'процентов')
+#     elif int(procent) % 10 == 1:
+#         print ((procent), 'процент')
+#     elif int(procent) % 10 > 1 and int(procent) % 10 <5:
+#         print ((procent), 'процента')
+#     else:
+#         print ((procent), 'процентов')
